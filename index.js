01201d8a6b05cb7723daffcdb9969fb1b9a2f8f5
sabuj-mojumdar <mojumdarnb@gmail.comm>
2021-11-26 09:32:29 +0600

    code edit

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello Welcome to komo bike Service')
+    res.send('Hello Welcome to komo bike Service website')
 })
 
 app.listen(port, () => {
